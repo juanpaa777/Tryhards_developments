8eb962394f87d409354d423184b6258dc5c330f5
juanpaa777 <156286909+juanpaa777@users.noreply.github.com>
2024-10-08 13:51:33 -0600

    oficial 2

---
+++ Cliente/src/app/components/home/login/auth-google.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGoogleService {
+  constructor(private oauthService: OAuthService) {
+    this.initLogin();
+  }
+
+  initLogin() {
+    const config: AuthConfig = {
+      issuer: 'https://accounts.google.com',
+      strictDiscoveryDocumentValidation: false,
+      clientId: '158523788954-s2bifoh0e1ilqhftkupkfitq9g9pbkau.apps.googleusercontent.com',
+      redirectUri: window.location.origin + '/main',
+      scope: 'openid profile email',
+    };
+
+    this.oauthService.configure(config);
+    this.oauthService.setupAutomaticSilentRefresh();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+  login() {
+    this.oauthService.initLoginFlow();
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+
+  getProfile() {
+    return this.oauthService.getIdentityClaims();
+  }
+}
